975c3e6e706fcd1ee41315b02b8001107a336b3e
ldevillard <legoexpliation@gmail.com>
2020-12-09 11:55:56 +0100

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # get_next_line
+### My get_next_line project at Cursus 42 in December 2020
+
+##### The GNL_BONUS doesn't work [TIMED OUT] but the code has one static variable -> TOTAL : 107
+Enjoy !
+
+| Function      | status|
+| ------------- |-------|
+| GNL           | OK    |
+| ONE STATIC    | OK    |
+| GNL_BONUS     | KO    |
---
+++ main.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ldevilla <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/03 13:40:26 by ldevilla          #+#    #+#             */
+/*   Updated: 2020/12/04 11:47:35 by ldevilla         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+int		main(int argc, char **argv)
+{
+	int		fd;
+	char	*line;
+
+	if (argc == 1)
+		fd = 0;
+	else if (argc == 2)
+		fd = open(argv[1], O_RDONLY);
+	else
+		return (2);
+	while (get_next_line(fd, &line) == 1)
+	{
+		printf("%s\n", line);
+		free(line);
+	}
+	if (argc == 2)
+		close(fd);
+}
